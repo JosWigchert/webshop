adc8abbc312231fde0262b2e231b3ec24e081a49
joswigchert <jwigcher@avans.nl>
2020-06-11 15:16:37 +0200

    updated

--- common/var.php
+++ common/var.php
@@ -1,8 +1,8 @@
 <?php
     // Database credentials
-    define('DB_HOST', 'db.joswigchert.nl');
-    define('DB_PORT', '3306');
-    define('DB_USER', 'md527402db510106');
-    define('DB_PASS', '123Jo$456');
-    define('DB_NAME', 'md527402db510106');
+    define('DB_HOST', '...');
+    define('DB_PORT', '...');
+    define('DB_USER', '...');
+    define('DB_PASS', '...');
+    define('DB_NAME', '...');
 ?>
